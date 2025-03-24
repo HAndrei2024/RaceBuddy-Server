b420e24d185433e126ead24abd115a57ba121778
Andrei Harja <harjaandrei@yahoo.com>
2025-03-24 16:44:53 +0200

    Python server: updated database, did not treat all cases

--- server.py
+++ server.py
@@ -13,16 +13,19 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_API_KEY)
 def upload_excel():
     if "file" not in request.files:
         return jsonify({"error": "No file part"}), 400
-    # if "columns" not in request.data:
-    #     return jsonify({"error": "No columns part"}), 400
-    # if "event_id" not in request.data:
-    #     return jsonify({"error": "No event_id part"}), 400
+    if "columns" not in request.form:
+        return jsonify({"error": "No columns part"}), 400
+    if "event_id" not in request.form:
+        return jsonify({"error": "No event_id part"}), 400
 
     file = request.files["file"]
     database_excel_columns_map = json.loads(request.form["columns"])
     event_id = request.form["event_id"]
     excel_required_columns = database_excel_columns_map.values()
-    print("It's working! #1")
+
+    final_response_dict = {"not_found" : []}
+    final_response_code = -1
+
     if not (file.filename.endswith('.xlsx') or not file.filename.endswith('.xls')):
         return jsonify({"error": "Invalid file format. Only .xlsx or .xls files are allowed."}), 400
     
@@ -31,7 +34,7 @@ def upload_excel():
 
         for sheet_name in xls.sheet_names:
             excel_data = pd.read_excel(file, sheet_name=sheet_name)
-            print("It's working! #2")
+            print("#1 Excel read succesfully!")
             if does_excel_sheet_contain_required_columns(excel_data, excel_required_columns):
                 for index, row in excel_data.iterrows():
                     update_query_dict = {}
@@ -40,40 +43,57 @@ def upload_excel():
                     # build a dict with the following structure:
                     # key (database columns): value (value from excel - from the required column)
                         if key != "name":
-                            update_query_dict[key] = row[database_excel_columns_map[key]]
-                            print("It's working! #3")
+                            update_query_dict[key] = str(row[database_excel_columns_map[key]])
+                            
                         else:
                             
                             athlete_name = row[database_excel_columns_map[key]]
                             print(f"{athlete_name=}")
                             athlete_id = get_athlete_id(athlete_name, supabase)
-                            print(f"{athlete_id=}")
-                            # update_query_dict["athlete_id"] = athlete_id
+                            
+                            result_dict_response = verify_athlete_result(athlete_id, event_id)
+ 
+                            if bool(result_dict_response) == False:
+                                final_response_dict["not_found"].append(athlete_name)
+                                
 
+                            print(f"{athlete_id=}")
                             if athlete_id == -1:
                                 print(f"Athlete {athlete_name} id couldn't be found!")
                     
                     if athlete_id != -1:
-                        response = update_database(update_query_dict, athlete_id, event_id, supabase)
-
-                        if response.status_code == 200:
-                            print("Update successful.")
+                        if bool(final_response_dict["not_found"]) == True:
+                            final_response_dict["message"] = "There are results in the excel not registered in the database!"
+                            final_response_code = 400
                         else:
-                            print(f"Error: {response.status_code} - {response.error_message}")
-            
-        return jsonify({"message": "Data successfully uploaded to Supabase."}), 200
+                            print("#2 Updating the database...")
+                            print(f"{update_query_dict=}")
+                            update_database(update_query_dict, athlete_id, event_id, supabase)
+
+        
+        final_response_dict["message"] = "Data successfully uploaded to Supabase."
+        return jsonify(final_response_dict), 200
     
     except Exception as e:
         return jsonify({"error": str(e)}), 500
 
 
+def verify_athlete_result(athlete_id, event_id):
+    response = supabase.table("Result").select("*") \
+                    .eq("athlete_id", athlete_id) \
+                    .eq("event_id", event_id) \
+                    .execute()
+
+    return response.data
+
+
 # assuming name like: first_name + last_name
 def get_athlete_id(name, supabase):
     response = supabase.table("Athlete").select("athlete_id") \
-                .eq('first_name', 'First') \
+                .eq('first_name', name) \
                 .execute()
-    print(response)
-    return response.data
+
+    return response.data[0]["athlete_id"]
 
 
 def does_excel_sheet_contain_required_columns(excel_data, excel_required_columns):
@@ -88,12 +108,13 @@ def does_excel_sheet_contain_required_columns(excel_data, excel_required_columns
 
     
 def update_database(query_dict, athlete_id, event_id, supabase):
+    print(f"{athlete_id=}")
     response = supabase.table("Result") \
                 .update(query_dict) \
                 .eq('athlete_id', athlete_id) \
                 .eq('event_id', event_id) \
                 .execute()
-    
+    print(f"Database updated! {response=}")
     return response
 
 
